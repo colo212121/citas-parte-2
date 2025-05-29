fba2f0a3e6a1e1d00d26cac381aa34bfea7243a0
ELIAS  BRODSKY <48387263@ort>
2025-05-29 10:15:37 -0300

    terminado

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Primer-TP-React-Parte-1/',
-  "homepage": 'https://github.com/colo212121/Primer-TP-React-Parte-1'
+  base: '/citas-parte-2/',
 })
